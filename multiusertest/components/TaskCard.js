44955d1e6acce792eb3e3fe25d797524ac4e448f
TonyGorgonzola <simon.huelskamp@gmail.com>
2022-07-31 19:18:20 +0200

    removed grid layout, started custom implementation
    cringe

--- multiusertest/components/TaskCard.js
+++ multiusertest/components/TaskCard.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function TaskCard() {
+function TaskCard(props) {
+
+  const [dragging, setDragging] = useState(false); 
+
+  const dragStarted = (e, id) => {
+    console.log("drag has started");
+    e.dataTransfer.setData("cardId", id);
+    setDragging(true);
+  }
+
   return (
-    <div className='text-white'>taskCard</div>
+    <div draggable onDragStart={(e) => {dragStarted(e, props.id)}} 
+      className='p-4 text-black w-full h-36 bg-white text-center shadow-md rounded-sm
+                hover:shadow-xl transition ease-in
+                cursor-drag' >
+        <h1 className='text-2xl font-light'>TaskCard {props.id}</h1>
+    </div>
   )
 }
 
--- multiusertest/pages/index.tsx
+++ multiusertest/pages/index.tsx
@@ -1,21 +1,43 @@
 import type { NextPage } from 'next'
+import { DragEvent } from 'react';
 import TaskCard from "../components/TaskCard"
-import GridLayout from "react-grid-layout";
+
 
 const Home: NextPage = () => {
+
+  const draggingOver = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log("draggingOver!!!")
+  }
+
+  const dropped = (e: DragEvent<HTMLDivElement>) => {
+    console.log("you have dropped");
+    let transferredCardId = e.dataTransfer.getData("cardId");
+    console.log( "card ID " + transferredCardId + " has been transferred")
+  }
+
   return (
   <>
-    <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
-        <div key="a" data-grid={{ x: 2, y: 0, w: 4, h: 2}} className=' w-10 h-5 bg-black cursor-grab rounded-md'>
-          <TaskCard />
+    <div className='m-4 grid grid-cols-2 gap-4'>
+      <div className='bg-green-400 min-h-screen min-w-full rounded-sm p-4 shadow-lg'>
+        <div className='bg-green-200 p-3 text-center rounded-sm shadow-lg'>
+          <h1 className=' text-3xl text-black font-bold pb-2'> COLUMN 1 </h1>
         </div>
-        <div key="b" data-grid={{ x: 1, y: 0, w: 4, h: 2}} className=' w-10 h-5 bg-black cursor-grab rounded-md'>
-          <TaskCard />
+        <div className='bg-green-200 w-full min-h-screen mt-4 p-4 rounded-sm shadow-lg'>
+          <TaskCard id={1}/>
         </div>
-        <div key="c" data-grid={{ x: 4, y: 0, w: 4, h: 2}} className=' w-10 h-5 bg-black cursor-grab rounded-md'>
-          <TaskCard />
+      </div>
+
+      <div className=' bg-fuchsia-600 min-h-screen min-w-full  rounded-sm p-4 shadow-lg'>
+        <div className=' p-3 bg-fuchsia-400 text-center rounded-sm shadow-lg'>
+          <h1 className=' text-3xl text-black font-bold pb-2'> COLUMN 2 </h1>
         </div>
-    </GridLayout>
+        <div className='bg-fuchsia-400 w-full min-h-screen mt-4 p-4 rounded-sm hadow-lg'
+            onDragOver={(e) => draggingOver(e)} onDrop={(e) => dropped(e)}>
+          <TaskCard id={2}/>
+        </div>
+      </div>
+    </div>
   </>
   )
 }
